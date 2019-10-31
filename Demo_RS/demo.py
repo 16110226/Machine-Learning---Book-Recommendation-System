28f9d979b942875b28da9f84ebd4c0a2481f1486
Tu Ngo <it.tunv@gmail.com>
2019-10-31 23:04:57 +0700

    Update comment code

--- Demo_RS/demo.py
+++ Demo_RS/demo.py
@@ -1,23 +1,25 @@
-# import MySQLdb
-# conn=MySQLdb.connect(host='localhost',user='root',passwd='1234',db='book_rs')
-# mycursor=conn.cursor()
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from IPython.core.display import clear_output
-from PIL._imaging import display
-from sklearn.neighbors import NearestNeighbors
+from IPython.core.display import clear_output, display
+from sklearn.neighbors import NearestNeighbors as k, NearestNeighbors
 from sklearn import metrics as metric
 import ipywidgets as widgets
 import io
+import re
 import sys
+from subprocess import STDOUT
 
 
+#Doc du lieu dau vao
+#Bang book
 books = pd.read_csv('BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
 books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrls', 'imageUrlM', 'imageUrlL']
+#Bang user
 users = pd.read_csv('BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
 users.columns = ['userID', 'Location', 'Age']
+#Bang rating
 ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
 ratings.columns = ['userID', 'ISBN', 'bookRating']
 print (books.shape)
@@ -29,6 +31,9 @@ print (ratings.shape)
 
 print(books.yearOfPublication.unique())
 print (users.head)
+
+#Tien xu ly du lieu
+#O cot nam XB, sua cac du lieu kieu string thanh kieu int
 print(books.loc[books.yearOfPublication== 'DK Publishing Inc',:])
 
 books.loc[books.yearOfPublication=='DK Publishing Inc',:]
@@ -50,17 +55,19 @@ books.loc[books.ISBN=='2070426769','yearOfPublication']="Peuple du ciel, suivi d
 
 books.yearOfPublication=pd.to_numeric(books.yearOfPublication,errors='coerce')
 
-books.loc[(books.yearOfPublication>2006)|(books.yearOfPublication==0),'yearOfPublication']=np.NAN
+#Sua cac nam xuat ban vo ly (Nhung nam > 2019 hoac =0)
+books.loc[(books.yearOfPublication>2019)|(books.yearOfPublication==0),'yearOfPublication']=np.NAN
 books.yearOfPublication.fillna(round(books.yearOfPublication.mean()),inplace=True)
 books.yearOfPublication= books.yearOfPublication.astype(np.int32)
 
 print(books.yearOfPublication.unique())
 
-
+#Them gia tri vao nhung cot null
 print (books.loc[books.publisher.isnull(),:])
 books.loc[(books.ISBN=='193169656X'),'publisher']='other'
 books.loc[(books.ISBN=='1931696993'),'publisher']='other'
 
+#Sua nhung user co tuoi vo ly (>90 hoac <5)
 users.loc[(users.Age>90)|(users.Age<5),'Age'] =np.nan
 users.Age=users.Age.fillna(users.Age.mean())
 users.Age=users.Age.astype(np.int32)
@@ -73,16 +80,19 @@ ratings_new = ratings_new[ratings_new.userID.isin(users.userID)]
 print (ratings.shape)
 print (ratings_new.shape)
 
+#Tinh do rai rac cua rating
 sparsity=1.0-len(ratings_new)/float(n_users*n_books)
 print('The sparsity level is ' + str(sparsity*100)+'%')
 
+#Hien len do thi cho biet so luong user danh gia sao cua sach
 ratings_explicit = ratings_new[ratings_new.bookRating !=0]
 ratings_implicit=ratings_new[ratings_new.bookRating==0]
 users_exp_ratings=users[users.userID.isin(ratings_explicit.userID)]
 users_imp_ratings=users[users.userID.isin(ratings_implicit.userID)]
 sns.countplot(data=ratings_explicit,x='bookRating')
-# plt.show()
+plt.show()
 
+#HIen len 10 quyen sach duoc danh gia cao nhat
 ratings_count=pd.DataFrame(ratings_explicit.groupby(['ISBN'])['bookRating'].sum())
 top10=ratings_count.sort_values('bookRating',ascending=False).head(10)
 print("Following books are recommended")
@@ -90,11 +100,13 @@ print(top10.merge(books,left_index=True,right_on='ISBN'))
 
 # COLLABORATIVE FILTERING BASED RECOMMENDATION SYSTEM
 
+#Vi ly do may, chi chon nhung quyen sach co hon 100 nguoi danh gia
 counts1 = ratings_explicit['userID'].value_counts()
 ratings_explicit = ratings_explicit[ratings_explicit['userID'].isin(counts1[counts1 >= 100].index)]
 counts = ratings_explicit['bookRating'].value_counts()
 ratings_explicit = ratings_explicit[ratings_explicit['bookRating'].isin(counts[counts >= 100].index)]
 
+#Tao 1 bang, ten hang la UserID, ten cot la ma sach va gia tri moi cot la so sao user danh gia ma sach do
 ratings_matrix = ratings_explicit.pivot(index='userID', columns='ISBN', values='bookRating')
 userID = ratings_matrix.index
 ISBN = ratings_matrix.columns
@@ -103,6 +115,7 @@ ratings_matrix.head()
 
 # USER-BASED CF
 
+#Su dung knn tim ra similar user
 def findksimilarusers(user_id, ratings, metric = metric, k=k):
     similarities=[]
     indices=[]
@@ -123,14 +136,14 @@ def predict_userbased(user_id, item_id, ratings, metric = metric, k=k):
     product=1
     wtd_sum = 0
     for i in range(0, len(indices.flatten())):
-        if indices.flattenfflil == user_loc:
+        if indices.flatten()[i] == user_loc:
             continue;
         else:
             ratings_diff = ratings.iloc[indices.flatten()[i],item_loc]-np.mean(ratings.iloc[indices.flatten()[i],:])
             product = ratings_diff * (similarities[i])
             wtd_sum = wtd_sum + product
         # fincase of very sparse datasets, using correlation metric for CF approach may give -ve ratings which are handled here as below:
-    if prediction >= 0:
+    if prediction <= 0:
         prediction = 1
     elif prediction >10:
         prediction = 10
@@ -142,7 +155,7 @@ def predict_userbased(user_id, item_id, ratings, metric = metric, k=k):
 
 def recommendItem(user_id, ratings, metric= metric):
     if (user_id not in ratings.index.values) or type(user_id) is not int:
-        print ("User id should be a valid integer from this list :\n\n {}".format(re.sub('[\[\]]','', np.array_str(ratings_))))
+        print("User id should be a valid integer from this list :\n\n {}".format(re.sub('[\[\]]','', np.array_str(ratings_matrix))))
     else:
         ids = ['Item-based (correlation)','Item-based (cosine)','User-based (correlation)','User-based (cosine)']
         select = widgets.Dropdown(options=ids, value=ids[0],description='Select approach', width='1000px')
@@ -176,7 +189,7 @@ def recommendItem(user_id, ratings, metric= metric):
         select.observe(on_change)
         display(select)
 
-recommendItem(4385, ratings_matrix)
+    recommendItem(4385, ratings_matrix)
 
 
 # ITEM-BASED CF
@@ -191,16 +204,15 @@ def findksimilarusers(item_id, ratings, metric = metric, k=k):
     similarities = 1-distances.flatten()
     return similarities,indices
 
-def predict_itembased(user_id, item_id, ratings, metric = metric, k=k):
+def predict_itembased(user_id, item_id, ratings, metric = metric, k = k):
     prediction = wtd_sum = 0
     user_loc = ratings.index.get_loc(user_id)
     item_loc = ratings.columns.get_loc(item_id)
-    similarities, indices=findksimilarusers(item_id, ratings)
-    mean_rating = ratings.iloc[user_loc,:].mean()
+    similarities, indices=findksimilaritems(item_id, ratings)
     sum_wt = np.sum(similarities)-1
     product=1
     for i in range(0, len(indices.flatten())):
-        if indices.flattenfflil == item_loc:
+        if indices.flatten()[i] == item_loc:
             continue;
         else:
             product = ratings.iloc[user_loc, indices.flatten()[i]] * (similarities[i])
